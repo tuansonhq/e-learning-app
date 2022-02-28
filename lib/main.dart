110d78f80c0db091d32d91a7ade31603fe9e1040
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-02-28 16:02:58 +0700

    Update

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:elearningapp/root_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(fontFamily: 'WorkSans'),
+    debugShowCheckedModeBanner: false,
+    home: RootApp(),
+  ));
+}
